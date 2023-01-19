231ed3b83caf6a185ecf0cc7df78b52f44b1f36c
rosendo <rosendc30@gmail.com>
2023-01-18 23:33:15 -0300

    mobile

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import './src/lib/dayjs'
+import { StatusBar } from "react-native";
 import {
   useFonts,
   Inter_400Regular,
@@ -9,6 +9,7 @@ import {
 } from "@expo-google-fonts/inter";
 
 import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,30 +24,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}> - - - - - - - - - - - - - - - - - - - - </Text>
-      <Text style={styles.text}>Abra app.tsx para iniciar a configuração do seu super app</Text>
-      <Text style={styles.text}> - - - - - - - - - - - - - - - - - - - - </Text>
-      <Text style={styles.text}>O app está usando a font Inter</Text>
-      <Text style={styles.text}> - - - - - - - - - - - - - - - - - - - - </Text>
-      
+    <>
+      <Home />      
       <StatusBar barStyle="light-content" backgroundColor={'transparent'} translucent/>
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#09090A",
-    alignItems: "center",
-    justifyContent: "center",
-    
-  },
-  text: {
-    color:"#7C3AED",
-    fontSize: 24,
-    fontFamily: 'Inter_400Regular',
-    textAlign: 'center'
-  },
-});
